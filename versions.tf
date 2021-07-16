aa0c87d6fcc43bfaa4f5a916d5fe95ba19ae9525
James Cooke <jamesedwardcooke@gmail.com>
2021-07-15 19:02:51 -0500

    Sane version constraints. Add providers.

--- versions.tf
+++ versions.tf
@@ -1,12 +1,21 @@
 terraform {
   required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.3.2, < 3.0.0"
+    }
     kubernetes-alpha = {
       source  = "hashicorp/kubernetes-alpha"
-      version = ">= 0.5.0"
+      version = ">= 0.5.0, < 1.0.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.1.0, < 4.0.0"
     }
   }
 
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.0.0, < 2.0.0"
 
   experiments = [module_variable_optional_attrs]
+
 }
