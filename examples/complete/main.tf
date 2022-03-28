c0c12f8a519b7428c4ff9e6f18224d1645547668
James Cooke <jamesedwardcooke@gmail.com>
2022-03-28 12:15:35 -0500

    bump example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -9,12 +9,12 @@ provider "kubernetes" {
 module "flux-install" {
   count   = 1
   source  = "OmniTeqSource/install/flux"
-  version = "0.0.24"
+  version = "0.1.4"
 }
 
 # Set to true after flux-install. GitRepository CRD need to be created before the repo instances may be created.
 locals {
-  install_complete = false
+  install_complete = true
 }
 
 module "git-repository-basic" {
@@ -40,7 +40,7 @@ module "git-repository-ssh" {
 
   create_ssh_key = true
   known_hosts = [
-    "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
+    "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
   ]
 
   # This will prevent a condition where the namespace cannot be removed if a CR for a CRD still exists.
