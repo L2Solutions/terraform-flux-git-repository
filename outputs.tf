3cf4cab42662f766ec0b9fe81773351a12b91020
James <jamesedwardcooke@gmail.com>
2022-05-17 14:12:06 -0500

    Optional suffix. Output kind/manifest/name/namespace. (#3)
    * Optional suffix. Output kind/manifest/name/namespace.
    
    * terraform-docs: automated action
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,22 @@
+output "manifest" {
+  value = local.manifest
+}
+
+output "name" {
+  description = "Name of the object with suffix(if enabled)"
+  value       = local.name
+}
+
 output "ssh_key" {
   description = "SSH public key for git integration"
   value       = local.create_ssh_key ? tls_private_key.this.0.public_key_openssh : null
+}
+
+output "kind" {
+  description = "Kubernetes API Kind"
+  value       = "GitRepository"
+}
+
+output "namespace" {
+  value = local.namespace
 }
